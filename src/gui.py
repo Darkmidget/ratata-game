ae3aa2b38b495238b4c580959f8d46497e404acd
Wata <forgotoffcom@gmail.com>
2023-11-30 16:10:27 +0800

    Created gui.py

--- src/gui.py
+++ src/gui.py
@@ -1,32 +1,67 @@
-input_dialogues = ["You have encountered a hobo that finds you cute.",
-                "He passed you a cheese and went on his way."]
-
-# Options
-input_options = ["1. Keep the cheese in your belonging",
-            "2. Eat the cheese"]
-
-
 import time
+import tkinter as tk
+from tkinter import messagebox
 from Ratata import Rat
-def visual(**kwargs):
-    for key, value in kwargs.items():
-        if key == "dialogues":
-            # dialogues = [list of dialogue]
-            for dialogue in value:
-                print(dialogue)
-                time.sleep(1)
-
-        if key == "rat":
-            for attribute, value1 in value.__dict__.items():
-                print(attribute, value1)
-
-        if key == "options":
-            # option =[list of choosable option]
-            for option in value:
-                print(option)
-                time.sleep(1)
-
 
 ## Testing Corner
-input_rat = Rat()
-visual(dialogues = input_dialogues,rat = input_rat, options = input_options)
+class RatGameApp:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Ratata")
+        self.master.geometry("1200x800")
+
+        self.dialogue_label = tk.Label(master, text="", wraplength=350)
+        self.dialogue_label.pack(pady=10)
+
+        self.stats_label = tk.Label(master, text="")
+        self.stats_label.pack(pady=10)
+
+        self.options_label = tk.Label(master, text="")
+        self.options_label.pack(padx=5)
+
+        ### --- Dummy code --- ###
+        dialogues = ["123", "Hello this is me"]
+        self.rat = Rat()
+        options = ["option1", "option2"]
+        self.update_display(dialogues, self.rat, options)
+        ### -------------------###
+
+    def clear(self):
+        pass
+
+    def update_display(self, dialogues, options):
+        self.clear()
+
+        self.dialogue_text = ""
+        for dialogue in dialogues:
+            self.dialogue_text += dialogue
+
+        # Update the labels
+        self.update_dialogue()
+        self.update_stats()
+
+    def update_dialogue(self):
+        self.dialogue_label.config(text=self.dialogue_text)
+
+    def update_stats(self, rat):
+        stats_text = "Rat Stats:\n"
+        for key, value in rat.__dict__.items():
+            stats_text += f"{key}: {value}\n"
+        self.stats_label.config(text=stats_text)
+
+    def create_option_buttons(self, options, functions):
+        self.option_buttons = []
+        for index, option in enumerate(options):
+            button = tk.Button(self.master, text=option, command=self.handle_option(index+1, functions(index)))
+            button.pack(pady=5)
+            self.option_buttons.append(button)
+
+    def handle_option(self, option_number, function):
+        """When pressed, return 1,2,3 or 4"""
+        self.rat = function(self.rat)
+
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    app = RatGameApp(root)
+    root.mainloop()
