aca07e0855f234ba1908a169f5b9b6e4978a7a76
aronsohgw <aron_soh@mymail.sutd.edu.sg>
2023-12-02 23:26:40 +0800

    create rat.py file for rat encounter

---
+++ src/rat.py
@@ -0,0 +1,11 @@
+import math
+import time
+from Ratata import Rat
+from dialogue import interact_handler
+
+#i have no idea what the fk im doing
+class otherRat()
+    def __init__(self):
+        ratata = 
+    
+    def rat_encounter(self, )
